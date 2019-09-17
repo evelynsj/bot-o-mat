a69cac65fddbde57b41da5f1e5620f160d5ca898
Evelyn Sjafii <evelynsj98@gmail.com>
2019-09-16 20:16:27 -0700

    Create constants file for robot types

---
+++ constants/RobotTypes.js
@@ -0,0 +1,11 @@
+export const UNIPEDAL = 'Unipedal'
+
+export const BIPEDAL = 'Bipedal'
+
+export const QUADRUPEDAL = 'Quadrupedal'
+
+export const ARACHNID = 'Arachnid'
+
+export const RADIAL = 'Radial'
+
+export const AERONAUTICAL = 'Aeronautical'
